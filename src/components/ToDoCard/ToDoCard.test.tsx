5a98a55951012d3f2a74290bb1cb5e6beb7d20b8
nary lozano <narylozano@gmail.com>
2023-05-10 21:03:20 +0200

    Add provider to ToDoCard test

--- src/components/ToDoCard/ToDoCard.test.tsx
+++ src/components/ToDoCard/ToDoCard.test.tsx
@@ -1,11 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import ToDoCard from "./ToDoCard";
 import { toDoMock } from "../../mocks/mock";
+import { Provider } from "react-redux";
+import { store } from "../../store";
 
 describe("Given the toDoCard component", () => {
   describe("When its rendered with a 'Ask Frank if its going to rain' toDo", () => {
     test("Then it should show a heading with 'Ask Frank if its going to rain'", () => {
-      render(<ToDoCard toDo={toDoMock} />);
+      render(
+        <Provider store={store}>
+          <ToDoCard toDo={toDoMock} />
+        </Provider>
+      );
       const toDoHeading = screen.getByRole("heading", {
         level: 2,
         name: toDoMock.name,
