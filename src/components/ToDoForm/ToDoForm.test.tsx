b48318bee8f1bcd724466f37d5b727848f92568d
nary lozano <narylozano@gmail.com>
2023-05-10 09:42:16 +0200

    Update setUpTest

--- src/components/ToDoForm/ToDoForm.test.tsx
+++ src/components/ToDoForm/ToDoForm.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
-import { store } from "../../store";
 import ToDoForm from "./ToDoForm";
+import { store } from "../../store";
 
 describe("Given a ToDoForm component", () => {
   describe("When is rendered", () => {
